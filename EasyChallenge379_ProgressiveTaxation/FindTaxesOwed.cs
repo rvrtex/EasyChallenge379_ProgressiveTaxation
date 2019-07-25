d98461c173afbd75e3f22062ecd0e37144ff5466
Matt <Mattmsj24@gmail.com>
2019-07-24 22:37:01 -0700

    Add project files.

---
+++ EasyChallenge379_ProgressiveTaxation/FindTaxesOwed.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyChallenge379_ProgressiveTaxation
+{
+    class FindTaxesOwed
+    {
+        public int findTaxesOwed(int income)
+        {
+            int taxableIncome = (int)income;
+            int taxesOwed = 0;
+            if (taxableIncome == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                var taxBrackets = getTaxBrackets();
+                for (int i = 0; i < taxBrackets.Count; i++)
+                {
+                    var currentTax = taxBrackets.ElementAt(i);
+
+                    if (taxableIncome >= currentTax.Value)
+                    {
+                        if (i != 0 && i + 1 < taxBrackets.Count)
+                        {
+                            taxesOwed += (int)((currentTax.Value - taxBrackets.ElementAt(i - 1).Value) * currentTax.Key);
+                        }
+                        else if (i == 0)
+                        {
+                            taxesOwed += (int)(currentTax.Value * currentTax.Key);
+                        }
+                        else if ((i + 1) >= taxBrackets.Count)
+                        {
+                            taxesOwed += (int)((taxableIncome - currentTax.Value) * currentTax.Key);
+                        }
+
+                    }
+                    else
+                    {
+                        if (i != 0)
+                        {
+                            if (taxableIncome > taxBrackets.ElementAt(i - 1).Value)
+                            {
+                                taxesOwed += (int)((taxableIncome - taxBrackets.ElementAt(i - 1).Value) * currentTax.Key);
+                            }
+                        }
+                        else
+                        {
+                            //TODO:throw exception that there are not enough tax brackets.
+                        }
+                    }
+                }
+
+            }
+            return taxesOwed;
+
+        }
+        public Dictionary<double, int> getTaxBrackets()
+        {
+            Dictionary<double, int> taxBrackets = new Dictionary<double, int>() { { 0.00, 10000 }, { 0.10, 30000 }, { 0.25, 100000 }, { 0.40, 100000 } };
+
+            return taxBrackets;
+        }
+
+    }
+}
+
+
+        /*
+         * if (taxableIncome >= currentTax.Value)
+                            {
+                                if ((i + 1) != taxBrackets.Count)
+                                {
+                                    if (taxableIncome >= taxBrackets.ElementAt(i + 1).Value)
+                                    {
+                                        if (currentTax.Key != 0.0 && i !=0 ) {
+                                            taxesOwed += (int)((currentTax.Value - taxBrackets.ElementAt(i - 1).Value) * currentTax.Key);
+                                        }
+                                        else
+                                        {
+                                            taxesOwed += (int)(currentTax.Value * currentTax.Key);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if(i != 0)
+                                        {
+                                            taxesOwed += (int)((taxableIncome - currentTax.Value) * taxBrackets.ElementAt(i + 1).Key);
+
+                                        }
+                                        else
+                                        {
+                                            if ((i + 1) != taxBrackets.Count)
+                                            {
+                                                taxesOwed += (int)((taxableIncome - currentTax.Value) * taxBrackets.ElementAt(i + 1).Key);
+                                            }
+                                            else
+                                            {
+                                                //TODO:throw exception that there are not enough tax brackets.
+                                            }
+
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    taxesOwed += (int)((taxableIncome - currentTax.Value) * currentTax.Key);
+                                }
+        */
+
---
+++ EasyChallenge379_ProgressiveTaxation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyChallenge379_ProgressiveTaxation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "";
+            while(input != "exit")
+            {
+                Console.WriteLine("please enter your income to calculate taxes owed. Type exit to Exit.");
+                input = Console.ReadLine();
+                int n;
+                if(int.TryParse(input,out n))
+                {
+                    var taxProgam = new FindTaxesOwed();
+                    var output = taxProgam.findTaxesOwed(n);
+                    Console.WriteLine("You owe: {0}",output);
+
+                }
+
+            }
+
+           
+        }
+
+        
+    }
+}
